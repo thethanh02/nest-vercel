ffb862d579014fea48e50fdc92080c76dfb1fd96
thethanh02 <2002tintin@gmail.com>
2024-06-10 22:30:35 +0700

    add vercel;

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,13 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { NoteService } from './note/note.service';
 import { NoteModule } from './note/note.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [UserModule, AuthModule, PrismaModule, NoteModule],
+  controllers: [AppController],
+  providers: [AppService],
   // controllers: [],
   // providers: [AuthService, NoteService],
 })
